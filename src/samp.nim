f7447cd70ea71a7d1730e8c419f4a401435ab0a9
jiro4989 <jiroron666@gmail.com>
2018-11-29 14:41:41 +0900

    [update]入力データのロジックのみ追加

--- src/samp.nim
+++ src/samp.nim
@@ -2,22 +2,35 @@ const doc = """
 samp is Sum Average Median Parcentile
 
 usage:
-  samp [-h|--help]
-  samp [-v|--version]
-  samp [-c|--count]
-       [-n|--min]
-       [-x|--max]
-       [-v|--average]
-       [-m|--median]
-       [-p|--parcentile]
+  samp [options]
+  samp [options] <filepath>...
+
+options:
+  -c --count        show count
+  -n --min          show minimum
+  -x --max          show maximum
+  -a --average      show average
+  -m --median       show median
+  -p --parcentile   show parcentile
+  -h --help         show this screen
+  -v --version      show version
 """
 
 import algorithm, math, docopt
 
 let args = docopt(doc, version = "v1.0.0")
-if args["-h"] or args["--help"]:
-  echo args
-  quit 0
+let files = args["<filepath>"]
+if files.len < 1:
+  # 標準入力を対象に処理
+  var line = stdin.read_line
+  var lines: seq[string] = @[]
+  while line != "":
+    lines.add line
+    line = stdin.read_line
+  echo lines
+else:
+  # ファイルを対象に処理
+  echo "file"
 
 proc parcentile*[T](x: open_array[T], m: int): float =
   let v = x.sorted(cmp)
